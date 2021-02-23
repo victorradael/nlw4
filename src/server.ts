a13a6397a7ba9b88ba71add979c2589ec3434541
Victor Radael <victorradael@hotmail.com>
2021-02-23 20:34:46 -0300

    :rocket: Database with TypeORM

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
+import "reflect-metadata";
 import express from "express";
+
+import "./database";
+import { router } from "./router";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Helloooooo!" });
-});
-
-app.post("/", (req, res) => {
-  return res.json({ message: "Deu certo!" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => {
   console.log("Server is Running!");
